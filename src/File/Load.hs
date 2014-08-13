a75cec030b387e4d8da1e47ae1c24a9d9d15c64c
isaev <valery.isaev@gmail.com>
2014-08-13 17:55:25 +0400

    Fix loading of modules

--- src/File/Load.hs
+++ src/File/Load.hs
@@ -7,6 +7,7 @@ import System.FilePath
 import Control.Monad
 import Control.Monad.Fix
 import Control.Monad.Trans
+import Control.Monad.State
 import Control.Exception
 import qualified Data.ByteString as B
 
@@ -19,18 +20,28 @@ import TypeChecking.Monad
 
 loadFile :: (MonadIO m, MonadFix m) => String -> ScopeT m ()
 loadFile filename = do
-    (errs, _) <- runWarnT $ do
-        mcnt <- liftIO $ fmap Right (B.readFile filename)
-                        `catch` \e -> return $ Left $ show (e :: SomeException)
-        case mcnt of
-            Right cnt -> parseDefs cnt
-            Left err  -> warn [emsg err enull]
+    (errs, _) <- runWarnT $ evalStateT (loadFile' [] filename) []
     liftIO $ mapM_ (hPutStrLn stderr . erenderWithFilename filename) errs
 
-parseDefs :: (MonadIO m, MonadFix m) => B.ByteString -> TCM m ()
-parseDefs s = do
-    defs <- pDefs s
-    lift $ forM_ (defs >>= \def -> case def of
+loadFile' :: (MonadIO m, MonadFix m) => [String] -> String -> StateT [String] (TCM m) ()
+loadFile' checking filename = do
+    mcnt <- liftIO $ fmap Right (B.readFile filename)
+                    `catch` \e -> return $ Left $ show (e :: SomeException)
+    case mcnt of
+        Right cnt -> parseDefs filename checking cnt
+        Left err  -> lift $ warn [emsg err enull]
+
+parseDefs :: (MonadIO m, MonadFix m) => String -> [String] -> B.ByteString -> StateT [String] (TCM m) ()
+parseDefs cur checking s = do
+    defs <- lift (pDefs s)
+    forM_ (defs >>= \def -> case def of
         DefImport imp -> [imp]
-        _             -> []) $ \moduleName -> loadFile $ foldr1 combine moduleName <.> "hoq"
-    typeCheckDefs defs
+        _             -> []) $ \moduleName ->
+            let filename = foldr1 combine moduleName <.> "hoq"
+            in if filename `elem` checking
+                then lift $ warn [emsg ("Modules " ++ cur ++ " and " ++ filename ++ " form a cycle") enull]
+                else do
+                    checked <- get
+                    if filename `elem` checked then return () else loadFile' (cur:checking) filename
+    modify (cur:)
+    lift (typeCheckDefs defs)
