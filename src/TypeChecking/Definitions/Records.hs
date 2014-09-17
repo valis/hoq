66c9f84442a0bb46092de47c4bd17ab7a2291d1d
isaev <valery.isaev@gmail.com>
2014-09-17 20:20:57 +0400

    Add type checking of record conditions

--- src/TypeChecking/Definitions/Records.hs
+++ src/TypeChecking/Definitions/Records.hs
@@ -1,30 +1,59 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module TypeChecking.Definitions.Records
     ( typeCheckRecord
     ) where
 
 import Data.Void
+import Control.Monad
 
-import Syntax
+import Syntax as S
 import Semantics
-import Semantics.Value
+import Semantics.Value as V
+import Semantics.Pattern as P
 import Syntax.ErrorDoc
 import TypeChecking.Monad
-import TypeChecking.Context
+import TypeChecking.Context as C
 import TypeChecking.Expressions.Utils
+import TypeChecking.Expressions.Patterns
 import TypeChecking.Expressions.Telescopes
+import TypeChecking.Expressions
 import Normalization
 
-typeCheckRecord :: Monad m => PName -> [Tele] -> Maybe PName -> [Field] -> [Clause] -> TCM m ()
-typeCheckRecord recPName@(recPos, recName) params mcon fields clauses = do
-    (SomeEq ctx, recType@(Type recTerm _)) <- typeCheckTelescope Nil params $ Type (universe Prop) (Set NoLevel)
+typeCheckRecord :: Monad m => PName -> [Tele] -> Maybe PName -> [Field] -> [S.Clause] -> TCM m ()
+typeCheckRecord recPName@(recPos, recName) params mcon fields conds = do
+    (SomeEq ctx, recType@(Type recTerm _)) <- typeCheckTelescope C.Nil params $ Type (universe Prop) (Set NoLevel)
     recID <- addDataTypeCheck recPName 1 $ Closed (vacuous recType)
-    (_, Type conType conSort) <- typeCheckTelescope ctx (map (\(Field pn e) -> VarsTele Explicit [pn] e) fields) $
+    (Fields ctx1 fields', Type conType conSort) <- typeCheckFields ctx fields $
         Type (Apply (Semantics (Name Prefix recName) $ DataType recID 1) $ ctxToVars ctx) Prop
-    case findOccurrence recID (nf WHNF conType) of
+    let ctx' = ctx C.+++ ctx1
+    forM_ fields' $ \(_, Type fieldType _) -> case findOccurrence recID (nf WHNF fieldType) of
         Just n | n > 0 -> throwError [Error Other $ emsgLC recPos "Record types cannot be recursive" enull]
         _ -> return ()
+    conds' <- forW conds $ \(S.Clause (pos, fn) pats expr) ->
+        case lookup fn fields' of
+            Just fieldType -> do
+                (bf, TermsInCtx ctx'' rtpats _ exprType) <- typeCheckPatterns ctx' fieldType pats
+                when bf $ warn [Error Other $ emsgLC pos "Absurd patterns are not allowed in conditions" enull]
+                (term, _) <- typeCheckCtx (ctx' C.+++ ctx'') expr $ Just (nfType WHNF exprType)
+                return $ Just (fn, (pos, P.Clause rtpats term))
+            Nothing -> do
+                warn [notInScope pos "record field" (nameToString fn)]
+                return Nothing
     case mcon of
         Just con -> addConstructorCheck con recID 0 [] $
             Closed $ Type (vacuous $ abstractTerm ctx $ replaceSort conType conSort Nothing) conSort
         _ -> return ()
-    lift $ replaceDataType recName 1 $ Closed $ Type (vacuous $ replaceSort recTerm (succ conSort) $ Just conSort) conSort
+
+data Fields b = forall a. Eq a => Fields (Ctx String (Type Semantics) b a) [(Name, Type Semantics a)]
+
+typeCheckFields :: (Monad m, Eq a) => Ctx String (Type Semantics) Void a
+    -> [Field] -> Type Semantics a -> TCM m (Fields a, Type Semantics a)
+typeCheckFields _ [] ty = return (Fields C.Nil [], ty)
+typeCheckFields ctx (Field (PIdent _ v) expr : exprs) ty = do
+    (term, Type t1 _) <- typeCheckCtx ctx expr Nothing
+    k1 <- checkIsType ctx (termPos expr) (nf WHNF t1)
+    (Fields ctx' terms, Type ty' k2) <- typeCheckFields (Snoc ctx v $ Type term k1) exprs (fmap Free ty)
+    let ctx'' = Snoc C.Nil v (Type term k1) C.+++ ctx'
+    return (Fields ctx'' $ (Ident v, fmap (liftBase ctx'') $ Type term k1) : terms,
+        Type (Apply (Semantics (S.Pi Explicit [v]) $ V.Pi k1 k2) [term, Lambda ty']) $ dmax k1 k2)
