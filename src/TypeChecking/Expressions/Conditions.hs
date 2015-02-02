valis/hoq

commit 90f721137bd61a98e8f766a19d41f2b7edc82d3c

Add conditions check to case

src/TypeChecking/Expressions/Conditions.hs
@@ -8,6 +8,7 @@ import Control.Monad
 import Control.Monad.State
 import Data.Maybe
 import Data.Bifunctor
+import Data.Void
 
 import qualified Syntax as S
 import Semantics
@@ -21,27 +22,28 @@ instance Eq (Term (s, Con t) a) where
     Var{} == Var{} = True
     _ == _ = False
 
-checkConditions :: S.Posn -> Closed (Term Semantics) -> [([Term (String,SCon) String], Closed (Term Semantics))] -> [EMsg (Term S.Syntax)]
-checkConditions lc func cs =
-    maybeToList $ msum $ map (\(p, scope) -> fmap msg $ checkPatterns func (map fst cs) p scope) cs
+checkConditions :: Eq a => S.Posn -> Ctx String f Void a => Term Semantics a
+    -> [([Term (String,SCon) String], Term Semantics a)] -> [EMsg (Term S.Syntax)]
+checkConditions pos ctx func cs =
+    maybeToList $ msum $ map (\(p, scope) -> fmap (msg ctx) $ checkPatterns func (map fst cs) p scope) cs
   where
-    msg :: ([String], Term Semantics String, Term Semantics String) -> EMsg (Term S.Syntax)
-    msg (vs, t1, t2) = emsgLC lc "Conditions check failed:" $ scopeToEDoc vs t1 <+> pretty "is not equal to" <+> scopeToEDoc vs t2
+    msg :: Ctx String f Void a -> ([String], Term Semantics a, Term Semantics a) -> EMsg (Term S.Syntax)
+    msg ctx (vs, t1, t2) = emsgLC pos "Conditions check failed:" $
+        scopeToEDoc ctx vs t1 <+> pretty "is not equal to" <+> scopeToEDoc ctx vs t2
     
-    scopeToEDoc :: [String] -> Term Semantics String -> EDoc (Term S.Syntax)
-    scopeToEDoc vs t = epretty $ bimap syntax pretty $ apps t (map cvar vs)
+    scopeToEDoc :: Ctx String f Void a -> [String] -> Term Semantics a -> EDoc (Term S.Syntax)
+    scopeToEDoc ctx vs t = epretty $ bimap syntax pretty $ apps (vacuous $ abstractTerm ctx t) $ map cvar (ctxVars ctx ++ vs)
 
 data TermInCtx  f b = forall a. TermInCtx  (Ctx String f b a) (f a)
 data TermsInCtx f b = forall a. TermsInCtx (Ctx String f b a) [f a]
 data TermsInCtx2 f b = forall a. TermsInCtx2 (Ctx String f b a) [f a] [f a]
 
-checkPatterns :: Closed (Term Semantics) -> [[Term (String,SCon) String]] -> [Term (String,SCon) String]
-    -> Closed (Term Semantics) -> Maybe ([String], Term Semantics String, Term Semantics String)
-checkPatterns (Closed func) cs pats (Closed scope) =
-    listToMaybe $ findSuspiciousPairs cs pats >>= \(TermsInCtx2 ctx terms terms') ->
-        let nscope1 = nfApps $ abstractTerm ctx (apps func terms)
-            nscope2 = abstractTerm ctx (apps scope terms')
-        in if nf NF nscope1 == nf NF nscope2 then [] else [(reverse $ ctxVars ctx, nscope1, nscope2)]
+checkPatterns :: Eq a => Term Semantics a -> [[Term (String,SCon) String]] -> [Term (String,SCon) String]
+    -> Term Semantics a -> Maybe ([String], Term Semantics a, Term Semantics a)
+checkPatterns func cs pats scope = listToMaybe $ findSuspiciousPairs cs pats >>= \(TermsInCtx2 ctx terms terms') ->
+    let nscope1 = nfApps $ abstractTerm ctx $ apps (fmap (liftBase ctx) func) terms
+        nscope2 = abstractTerm ctx $ apps (fmap (liftBase ctx) scope) terms'
+    in if nf NF nscope1 == nf NF nscope2 then [] else [(reverse $ ctxVars ctx, nscope1, nscope2)]
   where
     nfApps :: Eq a => Term Semantics a -> Term Semantics a
     nfApps (Apply a as) = Apply a $ map (nf WHNF) as
@@ -53,8 +55,8 @@ findSuspiciousPairs _ [] = []
 findSuspiciousPairs cs (pat@(Var var _) : pats) =
     check ILeft ++ check IRight ++ map ext (findSuspiciousPairs (mapTail pat cs) pats)
   where
-    ext (TermsInCtx2 ctx terms1 terms2) = TermsInCtx2 (Snoc ctx var $ error "") (cvar Bound : map (fmap Free) terms1)
-                                                                                (cvar Bound : map (fmap Free) terms2)
+    ext (TermsInCtx2 ctx terms1 terms2) = TermsInCtx2 (Snoc ctx var $ error "") (bvar : map (fmap Free) terms1)
+                                                                                (bvar : map (fmap Free) terms2)
     check con = if null $ filter (== Apply ("", ICon con) []) (mapHead cs)
         then []
         else case patternsToTerms pats of
@@ -117,7 +119,7 @@ substPatterns pats terms = evalState (mapM substPattern pats) terms
     substPattern Lambda{} = error "substPattern"
 
 patternToTerm :: Term (String,SCon) String -> TermInCtx (Term Semantics) a
-patternToTerm (Var var _) = TermInCtx (Snoc Nil var $ error "") (cvar Bound)
+patternToTerm (Var var _) = TermInCtx (Snoc Nil var $ error "") bvar
 patternToTerm (Apply (name, con) pats) = case patternsToTerms pats of
     TermsInCtx ctx' terms -> TermInCtx ctx' $ Apply (Semantics (S.Name S.Prefix $ S.Ident name) (Con con)) terms
 patternToTerm Lambda{} = error "patternToTerm"

src/Normalization.hs
@@ -45,7 +45,7 @@ nfSemantics mode t@(Semantics _ Coe) (t1:t2:t3:t4:ts) =
         (True, _, _, _) -> nfStep mode (apps t3 ts)
         (_, True, _, (Apply (Semantics _ Iso) [_,_,c,_,_,_])) -> nfStep mode $ apps c (t3:ts)
         (_, _, True, (Apply (Semantics _ Iso) [_,_,_,c,_,_])) -> nfStep mode $ apps c (t3:ts)
-        (_, b1, b2, _) | b1 || b2 -> case nf NF $ apps (fmap Free t1') [cvar Bound] of
+        (_, b1, b2, _) | b1 || b2 -> case nf NF $ apps (fmap Free t1') [bvar] of
             Apply (Semantics _ Iso) [c1, c2, c3, c4, c5, c6, Var Bound []] -> case map sequenceA [c1,c2,c3,c4,c5,c6] of
                 [Free{}, Free{}, Free c3', Free c4', Free{}, Free{}] -> nfStep mode $ apps (if b1 then c3' else c4') (t3:ts)
                 _ -> r
@@ -76,7 +76,7 @@ nfType :: Eq a => NF -> Type Semantics a -> Type Semantics a
 nfType mode (Type t lvl) = Type (nf mode t) lvl
 
 isStationary :: Eq a => Term Semantics a -> Bool
-isStationary t = case sequenceA $ nf NF $ apps (fmap Free t) [cvar Bound] of
+isStationary t = case sequenceA $ nf NF $ apps (fmap Free t) [bvar] of
     Free _  -> True
     Bound   -> False
 

src/Semantics.hs
@@ -35,7 +35,7 @@ instance Eq a => Eq (Term Semantics a) where
     Var a as == Var a' as' = a == a' && as == as'
     Lambda t == Lambda t' = t == t'
     Apply s@(Semantics _ Lam) [Lambda t] == Apply s'@(Semantics _ Lam) [Lambda t'] = Apply s [t] == Apply s' [t']
-    Apply s@(Semantics _ Lam) [Lambda t] == t' = Apply s [t] == apps (fmap Free t') [cvar Bound]
+    Apply s@(Semantics _ Lam) [Lambda t] == t' = Apply s [t] == apps (fmap Free t') [bvar]
     Apply (Semantics _ Lam) [t] == t' = t == t'
     t == t'@(Apply (Semantics _ Lam) _) = t' == t
     t@(Apply (Semantics _ Pi{}) _) == t'@(Apply (Semantics _ Pi{}) _) = pcompare t t' == Just EQ

src/Syntax/Term.hs
@@ -2,7 +2,7 @@
 
 module Syntax.Term
     ( Term(..), Scoped(..)
-    , capply, cvar, apps
+    , capply, cvar, bvar, apps
     , instantiate1
     , Closed(..), closed
     ) where
@@ -50,6 +50,9 @@ capply p = Apply p []
 
 cvar :: a -> Term p a
 cvar a = Var a []
+
+bvar :: Term p (Scoped a)
+bvar = cvar Bound
 
 apps :: Term s a -> [Term s a] -> Term s a
 apps t [] = t

src/TypeChecking/Definitions/DataTypes.hs
@@ -54,7 +54,8 @@ typeCheckDataType p@(pos, dt) params cons conds = mdo
                 warn [notInScope pos "data constructor" (getStr con)]
                 return Nothing
     lift $ replaceDataType dt lcons $ Closed $ Type (vacuous $ replaceSort dtTerm mk) mk
-    forM_ cons' $ \(PIdent pos _, (_, conds, con), _) -> warn $ checkConditions pos (Closed $ capply con) conds
+    forM_ cons' $ \(PIdent pos _, (_, conds, con), _) -> warn $
+        checkConditions pos Nil (capply con) $ map (\(p, Closed t) -> (p, t)) conds
 
 data SomeEq f = forall a. Eq a => SomeEq (f a)
 

src/TypeChecking/Definitions/Functions.hs
@@ -55,7 +55,7 @@ typeCheckFunction p@(pos, name) ety clauses = do
     case checkCoverage (map snd clausesAndPats) of
         Nothing | length clausesAndPats /= length (filter (\(_,_,me) -> isJust me) clauses) -> return ()
         r -> warn (coverageErrorMsg pos r)
-    warn $ checkConditions pos fc (map fst clausesAndPats)
+    warn $ checkConditions pos Nil (open fc) $ map (\((p,Closed t),_) -> (p,t)) clausesAndPats
 
 replaceFunCalls :: ID -> Closed (Term Semantics) -> Term Semantics a -> Term Semantics a
 replaceFunCalls name fc (Var a ts) = Var a $ map (replaceFunCalls name fc) ts

src/TypeChecking/Expressions.hs
@@ -98,8 +98,11 @@ typeCheckCtx ctx (Apply (pos, (S.Case (pat:pats))) (expr:terms)) mty = do
     (terms2', ty) <- typeCheckApps pos ctx terms2 type1'
     let (pats',terms1') = unzip patsAndTerms
         sem = Semantics (S.Case $ map (first $ \(s,_) -> ((0,0), Ident s)) $ pat':pats') $ V.Case (pat':pats')
+        terms' = term1' : terms1' ++ terms2'
     warn $ coverageErrorMsg pos $ checkCoverage $ zipWith (\p1 p2 -> (termPos p1, [first snd p2])) (pat:pats) (pat':pats')
-    return (Apply sem $ exprTerm : term1' : terms1' ++ terms2', ty)
+    warn $ checkConditions pos ctx (Lambda $ Apply sem $ bvar : map (fmap Free) terms') $
+        map (\(p,t) -> ([p],t)) $ (pat',term1'):patsAndTerms
+    return (Apply sem $ exprTerm:terms', ty)
   where
     isStationary :: Term a b -> Maybe (Term a b)
     isStationary (Lambda t) = case sequenceA t of
@@ -184,7 +187,7 @@ typeCheckKeyword ctx pos "path" (a:as) mty = do
             return (te, Type (Apply (pathImp k) [ty, te' ILeft, te' IRight]) k)
         Just (Type ty@(Apply (Semantics _ (Path k1)) [t1,_,_]) k) -> do
             let sem = Semantics (S.Pi ["i"]) $ V.Pi (TypeK NoLevel) k1
-            (r,t) <- typeCheckCtx ctx a $ Just $ Type (Apply sem [interval, Lambda $ apps (fmap Free t1) [cvar Bound]]) k1
+            (r,t) <- typeCheckCtx ctx a $ Just $ Type (Apply sem [interval, Lambda $ apps (fmap Free t1) [bvar]]) k1
             actExpType ctx (Apply (pathImp k1) [t1, apps r [iCon ILeft], apps r [iCon IRight]]) ty pos
             return (path [r], Type ty k)
         Just (Type ty _) -> throwError [emsgLC pos "" $ pretty "Expected type:" <+> prettyOpen ctx ty
@@ -193,11 +196,11 @@ typeCheckKeyword ctx pos "coe" [] _ = throwError [expectedArgErrorMsg pos "coe"]
 typeCheckKeyword ctx pos "coe" (a1:as) Nothing = do
     (r1, _, (v, t1)) <- typeCheckLambda ctx a1 intType
     k <- checkIsType (Snoc ctx v $ error "") (termPos a1) t1
-    let res = Apply (Semantics (S.Pi ["r"]) $ V.Pi (TypeK NoLevel) k) [interval, Lambda $ apps (fmap Free r1) [cvar Bound]]
+    let res = Apply (Semantics (S.Pi ["r"]) $ V.Pi (TypeK NoLevel) k) [interval, Lambda $ apps (fmap Free r1) [bvar]]
         coe = Semantics (Name Prefix $ Ident "coe") Coe
     case as of
         [] -> return (Apply coe [r1], Type (Apply (Semantics (S.Pi ["l"]) $ V.Pi (TypeK NoLevel) k) [interval, Lambda $
-            Apply (Semantics (S.Pi []) $ V.Pi k k) [apps (fmap Free r1) [cvar Bound], fmap Free res]]) k)
+            Apply (Semantics (S.Pi []) $ V.Pi k k) [apps (fmap Free r1) [bvar], fmap Free res]]) k)
         a2:as1 -> do
             (r2, _) <- typeCheckCtx ctx a2 (Just intType)
             case as1 of
@@ -222,7 +225,7 @@ typeCheckKeyword ctx pos "iso" (a1:a2:a3:a4:a5:a6:as) Nothing = do
     (r4, _) <- typeCheckCtx ctx a4 $ Just $ Type (Apply (Semantics (S.Pi []) $ V.Pi k2 k1) [r2,r1]) k
     let h e s1 s3 s4 tk = typeCheckCtx ctx e $ Just $ Type (Apply (Semantics (S.Pi ["x"]) $ V.Pi tk tk) [s1, Lambda $
             Apply (pathImp tk) [Apply (Semantics (S.Lam ["_"]) V.Lam) [Lambda $ fmap (Free . Free) s1],
-                apps (fmap Free s4) [apps (fmap Free s3) [cvar Bound]], cvar Bound]]) tk
+                apps (fmap Free s4) [apps (fmap Free s3) [bvar]], bvar]]) tk
         iso = Semantics (Name Prefix $ Ident "iso") Iso
     (r5, _) <- h a5 r1 r3 r4 k1
     (r6, _) <- h a6 r2 r4 r3 k2

src/TypeChecking/Expressions/Patterns.hs
@@ -45,14 +45,14 @@ typeCheckPattern ctx (Type ty@(Apply (Semantics _ (DataType dt _)) _) k) (Apply 
         (con@(Semantics _ (Con (DCon i n conds))), Closed (Type conType _)):_ -> if isDataType conType
             then return (False, Just $ TermInCtx Nil $ capply con, Apply (var, DCon i n conds) [])
             else throwError [notEnoughArgs pos var]
-        _ -> return (False, Just $ TermInCtx (Snoc Nil var $ Type ty k) $ cvar Bound, Var var [])
+        _ -> return (False, Just $ TermInCtx (Snoc Nil var $ Type ty k) bvar, Var var [])
   where
     isDataType :: Term Semantics a -> Bool
     isDataType (Lambda t) = isDataType t
     isDataType (Apply (Semantics _ DataType{}) _) = True
     isDataType _ = False
 typeCheckPattern ctx (Type ty k) (Apply (pos, Ident var) []) =
-    return (False, Just $ TermInCtx (Snoc Nil var $ Type ty k) $ cvar Bound, Var var [])
+    return (False, Just $ TermInCtx (Snoc Nil var $ Type ty k) bvar, cvar var)
 typeCheckPattern ctx (Type (Apply (Semantics _ (DataType dt _)) params) _) (Apply (pos, Ident conName) pats) = do
     cons <- lift $ getConstructor (Ident conName) (Just dt)
     case cons of
@@ -77,7 +77,7 @@ typeCheckPatterns ctx (Type (Apply p@(Semantics (S.Pi vs) (V.Pi l1 l2)) [a, b]) 
     (bf1, mte, rtpat) <- typeCheckPattern ctx a' pat
     TermInCtx ctx' te <- case mte of
                             Nothing ->  let var = if null vs then "_" else head vs
-                                        in return $ TermInCtx (Snoc Nil var a') (cvar Bound)
+                                        in return $ TermInCtx (Snoc Nil var a') bvar
                             Just te -> return te
     let b' = case b of
                 Lambda{} -> instantiate1 te $ fmap (fmap $ liftBase ctx') $ snd (dropOnePi p a b)

commit 479d82bda0946b18ee17b3b82a6202e43a12f2fa

Fix typo
Wrong arrow was introduced in 90f7211.

src/TypeChecking/Expressions/Conditions.hs
@@ -17,7 +17,7 @@ import TypeChecking.Context as C
 import TypeChecking.Expressions.Utils
 import Normalization
 
-checkConditions :: Eq b => Ctx String f Void b => Term Semantics b -> [(S.Posn, Clause b)] -> [Error]
+checkConditions :: Eq b => Ctx String f Void b -> Term Semantics b -> [(S.Posn, Clause b)] -> [Error]
 checkConditions ctx func cs = maybeToList $ msum $
     map (\(pos, Clause p scope) -> fmap (conditionsErrorMsg pos ctx) $ checkPatterns func (map (\(_, c) -> c) cs) p scope) cs
 